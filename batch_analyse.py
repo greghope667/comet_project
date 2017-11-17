d8af72fe7c744124d74757563df1bf377fccf777
greghope667 <greg.hope@virginmedia.com>
2017-11-17 17:34:55 +0000

    Add basic error logging.

--- batch_analyse.py
+++ batch_analyse.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 from analysis_tools_cython import *
-import os,sys
+import os
+import sys
+import traceback
 
 if len(sys.argv) > 0:
     directory = sys.argv[1]
 else:
-    print("Usage:",sys.argv[0],"[directory]")
+    print("Usage:",sys.argv[0],"[directory]",file=sys.stderr)
+
+if not os.path.isdir(directory):
+    print("Directory not found.",file=sys.stderr)
+    sys.exit()
 
 if not directory.endswith("/"):
     directory += "/"
+
 for fits_file in os.listdir(directory):
     if fits_file.endswith(".fits"):
         try:
@@ -20,6 +27,10 @@ for fits_file in os.listdir(directory):
             params = double_gaussian_curve_fit(T)
             ratio,separation = interpret(params)
             print(fits_file,ratio,separation,T.min())
+        except (KeyboardInterrupt, SystemExit):
+            raise
         except Exception as e:
-            pass
+            print(fits_file,file=sys.stderr)
+            traceback.print_exc()
+            print("/n",file=sys.stderr)
 
