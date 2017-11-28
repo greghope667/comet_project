af0e2cfa43bd0b61c4663aecdf75a3603b98a8ef
greghope667 <greg.hope@virginmedia.com>
2017-11-28 19:40:19 +0000

    Fixed multithreaded error catching

--- batch_analyse.py
+++ batch_analyse.py
@@ -21,21 +21,29 @@ parser.add_argument('-o',default='output.txt',dest='of',help='output file')
 
 def process_file(lock,of,path,f):
 
-    f_path = os.path.join(path,f)
-    table = import_lightcurve(f_path)
-    t,flux = clean_data(table)
-    flux = normalise_flux(flux)
-    flux = fourier_filter(flux,8)
-    T = test_statistic_array(flux,60)
-    params = double_gaussian_curve_fit(T)
-    ratio,separation = interpret(params)
-    s = skew(nonzero(T)) 
+    try:
+        f_path = os.path.join(path,f)
+        table = import_lightcurve(f_path)
+        t,flux = clean_data(table)
+        flux = normalise_flux(flux)
+        flux = fourier_filter(flux,8)
+        T = test_statistic_array(flux,60)
+        params = double_gaussian_curve_fit(T)
+        ratio,separation = interpret(params)
+        s = skew(nonzero(T)) 
 
-    result_str = ' '.join([f, str(ratio), str(separation), str(T.min()),str(s)])
-    lock.acquire()
-    with open(of,'a') as out_file:
-        out_file.write(result_str+'\n')
-    lock.release()
+        result_str = ' '.join([f, str(ratio), str(separation), 
+                                str(T.min()),str(s)])
+        lock.acquire()
+        with open(of,'a') as out_file:
+            out_file.write(result_str+'\n')
+        lock.release()
+    except (KeyboardInterrupt, SystemExit):
+        print("Process terminated early, exiting",file=sys.stderr)
+        raise
+    except Exception as e:
+        traceback.print_exc()
+        print("\n",file=sys.stderr)
 
 
 # Get directories from command line arguments.
@@ -60,14 +68,6 @@ for path in paths:
     fits_files = [f for f in os.listdir(path) if f.endswith('.fits')]
 
     process_partial = partial(process_file,l,args.of,path)
-    try:
-        pool.map(process_partial,fits_files)
-    except (KeyboardInterrupt, SystemExit):
-        print("Process terminated early, exiting",file=sys.stderr)
-        of.close()
-        pool.terminate()
-        sys.exit()
-    except Exception as e:
-        traceback.print_exc()
-        print("\n",file=sys.stderr)
+    
+    pool.map(process_partial,fits_files)
 
